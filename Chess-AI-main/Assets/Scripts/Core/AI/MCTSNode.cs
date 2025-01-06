638f2e9b0d2ab520a4450ed26758ac5f7bcc0714
Maty83 <matyashalir@gmail.com>
2025-01-06 15:56:09 +0100

    Everything before 06/01/25

--- Chess-AI-main/Assets/Scripts/Core/AI/MCTSNode.cs
+++ Chess-AI-main/Assets/Scripts/Core/AI/MCTSNode.cs
@@ -6,6 +6,47 @@
 
     public class MCTSNode
     {
+        //this gives me who moves next
+        public bool isBlackMove;
+
+        //We track all these since outside pure MCTS we can eval draws different.
+        public int wonPlayouts=0;
+        public int lostPlayouts=0;
+        public int drawPlayouts=0;
+
+        public MCTSNode prevNode;//My back-propagation hook if I don't wanna do recursion.
+
+        public int depth;//Depth is necessary if we want to adjust the balance of exploration of deep vs. unexplored moves.
+
+        public Board boardState;//I'd rather use a different one since this doesn't give board ambiguity between moves, but that just means it's a tree, not DAG (Both should work)
+        public Move lastMove;//Technically gettable from boardState, but I would at least want it as a getter method for ease of access.
+
+        //ADDED FOR SEARCH:
+        public Dictionary<Move, MCTSNode> children;
+
+        //public List<Move> validMoves; //This is used to store precomputed moves possible with a given board. Comparing if a move is in here or not would also give us unvisited moves.
+
+        public bool isTerminal;//Is this an end-state?
+
+        public MCTSNode(bool isBlackMove, MCTSNode prevNode, int depth, Board boardState, Move lastMove)
+        {
+            this.isBlackMove = isBlackMove;
+            this.prevNode = prevNode;
+            this.depth = depth;
+            this.boardState = boardState;
+            this.lastMove = lastMove;
+
+            children = new Dictionary<Move, MCTSNode>();
+            //validMoves = new List<Move>();
+            //TODO: Add isTerminal as a thing.
+        }
+
+        //Non-necessary items:
+
+        //Since simulation steps can possibly be done by a directing method rather than in node, we don't necessarily "need" the simulation of next step here (Actually, not having it is better)
+        //Same goes for any selection weighting since the necessary information is provided. But it might be more efficient to cache it here.
+
+
 
     }
 }
--- Chess-AI-main/Assets/Scripts/Core/AI/MCTSSearch.cs
+++ Chess-AI-main/Assets/Scripts/Core/AI/MCTSSearch.cs
@@ -1,7 +1,9 @@
 ﻿namespace Chess
 {
+    using Chess.Game;
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Threading;
     using UnityEngine;
     using static System.Math;
@@ -82,5 +84,126 @@
             searchStopwatch = System.Diagnostics.Stopwatch.StartNew();
             // Optional
         }
+
+        static MCTSNode DoSelection(MCTSNode root)
+        {
+            //Worst-case I have to wrap this somewhere, but for now we do it this way
+
+            //While we haven't reached a node with 0 children: Select node via UCB from done playouts and insert score of incomplete nodes.
+            //If the UCB of an unexplored node is higher, select a random node from the unexplored nodes.
+
+            const double drawWinMult = 0.5;
+            double explorationParam = 1;
+            var node = root;
+
+            while (0 == node.children.Count && !node.isTerminal)
+            {
+                //TODO:Select on UCB
+                double maxUCB = 0;
+                MCTSNode nextNode = null;
+                foreach (var kvp in node.children)
+                {
+                    MCTSNode child = kvp.Value;
+                    int totalPlayoutsParent = node.wonPlayouts + node.drawPlayouts + node.lostPlayouts;
+                    int totalPlayoutsChild = child.wonPlayouts + child.drawPlayouts + child.lostPlayouts;
+                    double currentUCB =
+                        (child.wonPlayouts + child.drawPlayouts * drawWinMult) / (totalPlayoutsChild) +
+                        explorationParam * Mathf.Sqrt(Mathf.Log(totalPlayoutsParent) / totalPlayoutsChild);//TODO: Unsure if this is the right UCB calculation
+
+                    if (currentUCB > maxUCB)
+                    {
+                        maxUCB = currentUCB;
+                        nextNode = child;
+                    }
+                }
+                node = nextNode;
+            }
+            if (node.isTerminal)
+            {
+                return node.prevNode;//TODO: We need to modify this for the terminal node presence!!!
+            }
+            else return node;
+
+            //I think creating a node and doing an expansion is the next homework, but I'm not sure.
+
+        }
+        static void expandNode(MCTSNode node)
+        {
+            //I will probably write a governing function later, so this doesn't need to be connected yet.
+
+            MoveGenerator moveGenerator = new MoveGenerator();
+
+            List<Move> children;
+            if (node.prevNode == null)
+            {
+                children = moveGenerator.GenerateMoves(node.boardState, true, true);
+            }
+            else
+            {
+                children=moveGenerator.GenerateMoves(node.boardState, false, true);
+            }
+            for (int i = children.Count - 1; i >= 0; i--)
+            {
+                Board boardCopy = node.boardState.Clone();
+                boardCopy.MakeMove(children[i], true);//Now we have the move.
+
+                MCTSNode newNode = new MCTSNode(boardCopy.ColourToMove == Piece.Black, node, node.depth + 1, boardCopy, children[i]);
+
+                if (GetResultFromBoard(boardCopy) != Result.Playing)
+                {
+                    newNode.isTerminal = true;
+                }
+                node.children.Add(children[i], newNode);
+            }
+        }
+        
+        //Following section is copied from the game manager since there was no other way to access the evaluation.
+        public enum Result { Playing, WhiteIsMated, BlackIsMated, Stalemate, Repetition, FiftyMoveRule, InsufficientMaterial, TooManyMoves }
+        static Result GetResultFromBoard(Board board)
+        {
+            MoveGenerator moveGenerator = new MoveGenerator();
+            var moves = moveGenerator.GenerateMoves_DO_NOT_USE(board);
+
+            // Look for mate/stalemate
+            if (moves.Count == 0)
+            {
+                if (moveGenerator.InCheck())
+                {
+                    return (board.WhiteToMove) ? Result.WhiteIsMated : Result.BlackIsMated;
+                }
+                return Result.Stalemate;
+            }
+
+            // Fifty move rule
+            if (board.fiftyMoveCounter >= 100)
+            {
+                return Result.FiftyMoveRule;
+            }
+
+            // Threefold repetition
+            int repCount = board.RepetitionPositionHistory.Count((x => x == board.ZobristKey));
+            if (repCount == 3)
+            {
+                return Result.Repetition;
+            }
+
+            // Look for insufficient material (not all cases implemented yet)
+            int numPawns = board.pawns[Board.WhiteIndex].Count + board.pawns[Board.BlackIndex].Count;
+            int numRooks = board.rooks[Board.WhiteIndex].Count + board.rooks[Board.BlackIndex].Count;
+            int numQueens = board.queens[Board.WhiteIndex].Count + board.queens[Board.BlackIndex].Count;
+            int numKnights = board.knights[Board.WhiteIndex].Count + board.knights[Board.BlackIndex].Count;
+            int numBishops = board.bishops[Board.WhiteIndex].Count + board.bishops[Board.BlackIndex].Count;
+
+            if (numPawns + numRooks + numQueens == 0)
+            {
+                if (numKnights == 1 || numBishops == 1)
+                {
+                    return Result.InsufficientMaterial;
+                }
+            }
+
+            return Result.Playing;
+        }
+    
     }
 }
